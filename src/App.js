da501f7efd85f6d2614fcaef8e8228d5ca14e4f2
Кирилл Тимофеев <kirilltim28122004@gmail.com>
2026-01-10 13:45:23 +0300

    дз2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import dayjs from 'dayjs';
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import dayjs from 'dayjs';
+// import axios from 'axios';
+
+// function App() {
+//   const [data, setData] = useState(null);
+
+//   const currentDate = dayjs().format('YYYY MM DD HH:mm:ss');
+
+//   useEffect(() => {
+//     axios.get('https://jsonplaceholder.typicode.com/posts/1')
+//       .then(response => setData(response.data))
+//       .catch(error => console.error(error));
+//   }, []);
+
+//   return (
+//     <div>
+//       {}
+//       <h1>Текущая дата и время:</h1>
+//       <p>{currentDate}</p>
+//       {}
+//       <div style={{ marginTop: '20px' }}>
+//         <h1>Данные из API:</h1>
+//         {data ? (
+//           <pre>{JSON.stringify(data, null, 2)}</pre>
+//         ) : (
+//           <p>Загрузка...</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useRef } from 'react';
+import Button from './components/Button';
+import Input from './components/Input';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  // Получаем текущую дату
-  const currentDate = dayjs().format('YYYY MM DD HH:mm:ss');
-
-  // Запрос к API
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts/1')
-      .then(response => setData(response.data))
-      .catch(error => console.error(error));
-  }, []);
-
-  return (
-    <div>
-      {/* Блок с текущей датой */}
-      <h1>Текущая дата и время:</h1>
-      <p>{currentDate}</p>
-
-      {/* Блок с данными из API */}
-      <div style={{ marginTop: '20px' }}>
-        <h1>Данные из API:</h1>
-        {data ? (
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        ) : (
-          <p>Загрузка...</p>
-        )}
+    const inputRef = useRef(null);
+    const handleClear = () => {
+        if (inputRef.current) {
+            inputRef.current.value = '';
+        }
+    };
+    return (
+      <div className="app-container">
+        <Input ref={inputRef} placeholder="Введите текст..." />
+        <Button onClick={handleClear} />
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;

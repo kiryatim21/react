e10dbc4030a73d822b982d842d11acc7207fed30
Кирилл Тимофеев <kirilltim28122004@gmail.com>
2026-01-10 13:32:28 +0300

    дз1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  // Получаем текущую дату
+  const currentDate = dayjs().format('YYYY MM DD HH:mm:ss');
+
+  // Запрос к API
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => setData(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Блок с текущей датой */}
+      <h1>Текущая дата и время:</h1>
+      <p>{currentDate}</p>
+
+      {/* Блок с данными из API */}
+      <div style={{ marginTop: '20px' }}>
+        <h1>Данные из API:</h1>
+        {data ? (
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        ) : (
+          <p>Загрузка...</p>
+        )}
+      </div>
     </div>
   );
 }

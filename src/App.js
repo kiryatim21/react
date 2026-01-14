b4c263f79b5da4a5f56a4f0b4ce7d1ca652e9e25
Кирилл Тимофеев <kirilltim28122004@gmail.com>
2026-01-14 21:54:39 +0300

    дз4

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
-// import React, { useEffect, useState } from 'react';
-// import dayjs from 'dayjs';
-// import axios from 'axios';
-
-// function App() {
-//   const [data, setData] = useState(null);
-
-//   const currentDate = dayjs().format('YYYY MM DD HH:mm:ss');
-
-//   useEffect(() => {
-//     axios.get('https://jsonplaceholder.typicode.com/posts/1')
-//       .then(response => setData(response.data))
-//       .catch(error => console.error(error));
-//   }, []);
-
-//   return (
-//     <div>
-//       {}
-//       <h1>Текущая дата и время:</h1>
-//       <p>{currentDate}</p>
-//       {}
-//       <div style={{ marginTop: '20px' }}>
-//         <h1>Данные из API:</h1>
-//         {data ? (
-//           <pre>{JSON.stringify(data, null, 2)}</pre>
-//         ) : (
-//           <p>Загрузка...</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button';
-import Input from './components/Input';
 import './App.css';
 
 function App() {
-    const inputRef = useRef(null);
-    const handleClear = () => {
-        if (inputRef.current) {
-            inputRef.current.value = '';
-        }
+    const [clickCount, setClickCount] = useState(0);
+    const handleButtonClick = () => {
+        setClickCount(clickCount + 1);
     };
     return (
-      <div className="app-container">
-        <Input ref={inputRef} placeholder="Введите текст..." />
-        <Button onClick={handleClear} />
-      </div>
+        <div
+            className="app-container"
+            style={{
+                backgroundColor: clickCount % 2 === 0 ? '#f4f4f4' : '#ffebcd',
+            }}
+        >
+            <Button onClick={handleButtonClick} />
+        </div>
     );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Button.css';
 
 const Button = ({ onClick }) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+        onClick();
+    };
     return (
-        <button className="custom-button" onClick={onClick}>
-            Очистить
+        <button
+            className={`custom-button ${isClicked ? 'clicked' : ''}`}
+            onClick={handleClick}
+        >
+            Изменить цвет
         </button>
     );
 };
